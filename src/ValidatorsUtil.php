2a60ca5512b3ec52d4be09967303679751c313eb
Martin Brettschneider <martin.brettschneider@adbros.cz>
2020-09-02 13:12:03 +0200

    ValidatorsUtil

---
+++ src/ValidatorsUtil.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace Adbros\Utils;
+
+use Nette\Utils\Strings;
+
+class ValidatorsUtil
+{
+
+	/**
+	 * Verifies that the value is valid IP address
+	 */
+	public static function isIp(string $value): bool
+	{
+		return filter_var($value, FILTER_VALIDATE_IP) !== false;
+	}
+
+	/**
+	 * Verifies that the value is strong password (contains 8 characters, 1 lowercase letter, 1 uppercase latter and 1 digit)
+	 */
+	public static function isPasswordStrong(string $value): bool
+	{
+		return Strings::match($value, '/^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/') !== null;
+	}
+
+}
